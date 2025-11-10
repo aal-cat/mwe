81369c1a3f280311830923622890b6c8985288b1
Astt123 <gszgxgxvx@gmail.com>
2025-11-10 16:33:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const heroes = [
-  { name: "عبدالله", title: "😺E=mc²😺", img: "1.jpg", link: "https://www.tiktok.com/@124hht?_r=1&_t=ZS-91HcIekKG8N" },
-  { name: "عبيدة", title: "كاريزما إلى الحد الأقصى", img: "2.jpg", link: "https://www.tiktok.com/@level.201?_r=1&_t=ZS-91HcKadGoS7" },
-  { name: "جاسم", title: "مازوخي", img: "3.jpg", id: "bachira_402" },
+  { name: "عبدالله", title: "😺E=mc²😺", img: "2.jpg", link: "https://www.tiktok.com/@124hht?_r=1&_t=ZS-91HcIekKG8N" },
+  { name: "عبيدة", title: "كاريزما إلى الحد الأقصى", img: "1.jpg", link: "https://www.tiktok.com/@level.201?_r=1&_t=ZS-91HcKadGoS7" },
+  { name: "جاسم", title: "مازوخي", img: "2.jpg", id: "bachira_402" },
   { name: "محمد", title: "", img: "4.jpg", link: "https://www.tiktok.com/@.999qaa?_r=1&_t=ZS-91HcOmodbtv" },
   { name: "غير مهم", title: "", img: "5.jpg", link: "https://www.tiktok.com/@red908287?_r=1&_t=ZS-91GZ2JWmj4x" },
   { name: "غير مهم", title: "", img: "6.jpg", link: "https://www.tiktok.com/@zxaspo?_r=1&_t=ZS-91GZ5U0JZGp" },
